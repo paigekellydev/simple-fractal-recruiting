e208254d15fb76e454f149b8f73a511f1cd28e0d
Paige <72361304+pckellyco@users.noreply.github.com>
2022-01-12 15:58:47 -0700

    created welcome component

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Welcome() {
+    return (
+        <div id="" className="app-page">
+            <h2 id="welcome-header" className='header-text'>Welcome</h2>
+            <label className="search-label">Please enter your candidate id</label>
+            <input type="text"></input>
+        </div>
+    )
+}
